0ee3a9fcf1321f4c5057f60affc29ac7277286b6
aidan <89723084+aidan-ngiau@users.noreply.github.com>
2023-09-24 02:02:30 +0800

    Add files via upload

---
+++ homework_week09.js
@@ -0,0 +1,4 @@
+var vg_1 = "week9.vg.json";
+vegaEmbed("#homework_week09_chart", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
